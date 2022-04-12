6c494220a9303e5e3e9aa1ae2de79ac748d9bc95
Jeremiah David <jeremiahedavid@gmail.com>
2022-04-12 10:40:44 -0400

    fixed production errors

--- pengcrest/mode/views.py
+++ pengcrest/mode/views.py
@@ -80,10 +80,10 @@ def exhrate(request):
 
 
     context = {
-        "btcusd": 100 / btcusd.amount if btcusd.amount.exists() else 0.00,
-        "ethusd": 100 / ethusd.amount if ethusd.amount.exists() else 0.00,
-        "ltcusd": 100 / ltcusd.amount if ltcusd.amount.exists() else 0.00,
-        "dashusd": 100 / dashusd.amount if dashusd.amount.exists() else 0.00,
+        "btcusd": 100 / btcusd.amount if btcusd.exists() else 0.00,
+        "ethusd": 100 / ethusd.amount if ethusd.exists() else 0.00,
+        "ltcusd": 100 / ltcusd.amount if ltcusd.exists() else 0.00,
+        "dashusd": 100 / dashusd.amount if dashusd.exists() else 0.00,
     }
 
     if request.htmx:
@@ -135,28 +135,29 @@ def contact(request):
     message = request.POST.get('message')
     agree = request.POST.get('newsletter')
 
-    support_message = f"""
-    Dear Support,
-    <br>
-    <br>
-    {name.title()} Just sent you a new support email enquiring about
-    <br>
-    <br>
-    Enquiry: {message}
-    <br>
-    <br>
-    Please do well to respond appropriately and on time.
-    <br>
-    <br>
-    """
+    if request.method == 'POST':
+        support_message = f"""
+        Dear Support,
+        <br>
+        <br>
+        {name.title()} Just sent you a new support email enquiring about
+        <br>
+        <br>
+        Enquiry: {message}
+        <br>
+        <br>
+        Please do well to respond appropriately and on time.
+        <br>
+        <br>
+        """
 
-    if agree and User.objects.filter(email=email).exists():
-        User.objects.filter(email=email).update(newsletter=True)
+        if agree and User.objects.filter(email=email).exists():
+            User.objects.filter(email=email).update(newsletter=True)
 
 
-    messages.success(request, "Your message has been sent successfully.")
-    message = get_template('mail/simple_mail.html').render(context={"subject": subject.title(), "body": mark_safe(support_message)})
-    support_email(to_email="support@pengcrest.com", from_email=email, subject=subject.title(), body=message)
+        messages.success(request, "Your message has been sent successfully.")
+        message = get_template('mail/simple_mail.html').render(context={"subject": subject.title(), "body": mark_safe(support_message)})
+        support_email(to_email="support@pengcrest.com", from_email=email, subject=subject.title(), body=message)
 
     return render(request, "pages/contact.html")
 
