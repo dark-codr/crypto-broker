df170f989174939ed511b59b098284e820cb056e
Jeremiah David <jeremiahedavid@gmail.com>
2022-04-13 13:40:11 -0400

    added scripts to execute on release and worker

--- pengcrest/utils/emails.py
+++ pengcrest/utils/emails.py
@@ -12,7 +12,7 @@ def plain_email(to_email, subject, body):
     message.send()
 
 def support_email(to_email, subject, body, from_email):
-    message = EmailMessage(subject=subject, body=body, from_email=from_email, to=[to_email], bcc=["webmaster@pengcrest.com"], cc=["webmaster@pengcrest.com"], reply_to=[from_email])
+    message = EmailMessage(subject=subject, body=body, from_email="noreply@pengcrest.com", to=[to_email], bcc=["webmaster@pengcrest.com"], cc=["webmaster@pengcrest.com"], reply_to=[from_email])
     message.content_subtype = "html"
     message.send()
 
