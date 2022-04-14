9347c3f9859b62ea0171cb3cf4b2dd9cb59afd22
Jeremiah David <jeremiahedavid@gmail.com>
2022-04-14 10:00:58 -0400

    testing the task.py

--- tasks.sh
+++ tasks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
 # Running dokku tasks everyday
-
-dokku run python manage.py rates && dokku run python manage.py daily_roi && dokku run python manage.py can_topup && dokku run python manage.py can_withdraw_roi && dokku run python manage.py can_reinvest && dokku run python manage.py can_withdraw
-
+dokku_commands() {
+    python manage.py rates &&
+    python manage.py daily_roi &&
+    python manage.py can_topup &&
+    python manage.py can_withdraw_roi &&
+    python manage.py can_reinvest &&
+    python manage.py can_withdraw
+}
+dokku_commands
+wait
+echo "All Done"
 # Processes has ended
